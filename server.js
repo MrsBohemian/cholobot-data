1de50b55a402a124441e38980bc29f22c7536656
MrsBohemian <handley.heaven@gmail.com>
2026-04-12 17:05:04 -0500

    Create server.js
    add express data service with calendar and tasks routes

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let calendar = {};
+let tasks = {};
+
+app.get("/", (req, res) => {
+  res.send("cholobot-data running");
+});
+
+app.get("/calendar", (req, res) => res.json(calendar));
+app.post("/calendar", (req, res) => {
+  calendar = req.body;
+  console.log("Updated calendar:", calendar);
+  res.json({ ok: true });
+});
+
+app.get("/tasks", (req, res) => res.json(tasks));
+app.post("/tasks", (req, res) => {
+  tasks = req.body;
+  console.log("Updated tasks:", tasks);
+  res.json({ ok: true });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Data service running");
+});
